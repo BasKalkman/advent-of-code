8852f64eaa325acbf586cc52ed521b7700eab60f
BasKalkman <b_kalkman@hotmail.com>
2021-10-28 00:08:46 +0200

    2015 Day 7

--- 2015/Day07/solution.js
+++ 2015/Day07/solution.js
@@ -1,67 +1,59 @@
 const fs = require('fs');
-// const data = fs.readFileSync('./input.txt', 'utf-8').split('\n');
-const data = fs.readFileSync('./example.txt', 'utf-8').split('\n')
-
-const wireValues = new Map()
-
-// Populate wire values with null
-for (line of data) {
-  const names = line.match(/[a-z]+/g)
-  for (let name of names) {
-    if (!wireValues.has(name)) {
-      wireValues.set(name, null)
-    }
-  }
-}
-
-
-
+const data = fs.readFileSync('./input.txt', 'utf-8').split('\n');
+// const data = fs.readFileSync('./example.txt', 'utf-8').split('\n')
 
 // Replace written bitwise operator with actual one for eval
 const fillOperation = (str) => {
-  if (str.match(/AND/)) {return str.replace('AND', '&')}
-  if (str.match(/OR/)) {return str.replace('OR', '|')}
-  if (str.match(/LSHIFT/)) {return str.replace('LSHIFT', '<<')}
-  if (str.match(/RSHIFT/)) {return str.replace('RSHIFT', '>>>')}
-  if (str.match(/NOT/)) {return str.replace('NOT', '~')}
+  if (str.match(/AND/)) { return str.replace('AND', '&') }
+  if (str.match(/OR/)) { return str.replace('OR', '|') }
+  if (str.match(/LSHIFT/)) { return str.replace('LSHIFT', '<<') }
+  if (str.match(/RSHIFT/)) { return str.replace('RSHIFT', '>>>') }
+  if (str.match(/NOT/)) { return str.replace('NOT', '~') }
+  return str;
 }
 
-
-const runWires = () => {
-  for (let line of data) {
-    const names = line.match(/[a-z]+/g)
-    const output = names.pop();
-    const knownValues = names.map(e => wireValues.get(e))
-    if (knownValues.includes(null)) {
-      break;
+// Setup wireValues store
+let wireValues = new Map();
+const fillWireValues = () => {
+  for (const line of data) {
+    const [input, target] = line.split('->').map(e => e.trim());
+    if (!input.match(/AND|OR|LSHIFT|RSHIFT|NOT/g) && !input.match(/[a-z]+/)) { // If input is a number, set target to number
+      wireValues.set(target, parseInt(input))
     } else {
-
-      // Prepare instruction for eval()
-      let instruction = line.split('->')[0].trim();
-      instruction = fillOperation(instruction);
-      // Change wire names to wire values
-      for (let i = 0; i < names.length; i++) {
-        instruction = instruction.replace(names[i], knownValues[i])
-      }
-  
-      // Run eval()
-      console.log(instruction)
-      let result = eval(instruction)
-      if (typeof result !== 'number') {
-        result = parseInt(result);
-      }
-  
-      wireValues.set(output, result)
+      wireValues.set(target, input);
     }
-    
   }
 }
+fillWireValues();
 
-const findTargetWireValue = (target) => {
-  while (wireValues.get(target) === null) {
-    runWires();
+
+const findWireValue = (wire) => {
+  let currentWireValue = wireValues.get(wire)
+  if (typeof currentWireValue === 'number') {
+    return currentWireValue;
   }
-  return wireValues.get(target);
+
+  // Prepare to eval operation
+  currentWireValue = fillOperation(currentWireValue);
+  const wiresInOperation = currentWireValue.match(/[a-z]+/g);
+
+  for (let i = 0; i < wiresInOperation.length; i++) {
+    currentWireValue = currentWireValue.replace(wiresInOperation[i], findWireValue(wiresInOperation[i]))
+  }
+
+  const result = eval(currentWireValue);
+  wireValues.set(wire, result);
+
+  return result;
 }
 
-findTargetWireValue('a')
+const part1 = findWireValue('a');
+console.log(`Part 1: ${part1}`);
+
+// Part 2
+wireValues.clear();
+fillWireValues();
+wireValues.set('b', part1);
+
+const part2 = findWireValue('a');
+console.log(`Part 2: ${part2}`);
