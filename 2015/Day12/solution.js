8e39859cd8837a3b7fa2c3535a03b962a0b22794
BasKalkman <b_kalkman@hotmail.com>
2021-11-02 23:32:50 +0100

    Day 12 Part 2

--- 2015/Day12/solution.js
+++ 2015/Day12/solution.js
@@ -6,3 +6,31 @@ const nums = data.match(/-?\d+/g).reduce((a, c) => {
 }, 0)
 
 console.log(nums);
+
+// Part 2
+const parsed = JSON.parse(data);
+
+let result = 0;
+
+const checkJSON = (value) => {
+    if (typeof value === 'number') {
+        result += value;
+        return;
+    }
+
+    if (Array.isArray(value)) {
+        value.map(e => checkJSON(e));
+        return;
+    }
+
+    if (typeof value === 'object' && !Array.isArray(value)) {
+        let valuesInObject = Object.values(value);
+        if (!valuesInObject.includes('red')) {
+            valuesInObject.map(e => checkJSON(e));
+        }
+    }
+}
+
+Object.values(parsed).map(e => checkJSON(e));
+
+console.log(result);
