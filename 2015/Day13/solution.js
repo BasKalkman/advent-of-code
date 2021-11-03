6656d34bc7eddc5b516d481dfbdfbd21ef29e4e3
BasKalkman <b_kalkman@hotmail.com>
2021-11-03 15:37:11 +0100

    Day 13 Part 2

--- 2015/Day13/solution.js
+++ 2015/Day13/solution.js
@@ -29,3 +29,33 @@ const seatingResult = permutations.map(e => {
 })
 
 console.log(Math.max(...seatingResult))
+
+// Part 2
+for (let person of Object.keys(matrix)) {
+    if (!matrix['self']) {
+        matrix['self'] = {}
+    }
+
+    matrix['self'][person] = 0;
+    matrix[person]['self'] = 0;
+}
+
+const permutationsP2 = permuteArray(Object.keys(matrix));
+const seatingResultP2 = permutationsP2.map(e => {
+    let result = 0;
+    for (let i = 0; i < e.length; i++) {
+        const [person1, person2] = [e[i], e[(i + 1) % e.length]]
+
+        result += matrix[person1][person2];
+        result += matrix[person2][person1];
+    }
+    return result;
+})
+
+let max = 0;
+for (let result of seatingResultP2) {
+    if (result > max) {
+        max = result
+    }
+}
+console.log(max)
