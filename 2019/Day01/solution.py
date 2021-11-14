a0992b50d41facffafe222c530232b49b93d0771
BasKalkman <b_kalkman@hotmail.com>
2021-11-14 20:25:29 +0100

    Day 1 refactor

--- 2019/Day01/solution.py
+++ 2019/Day01/solution.py
@@ -1,29 +1,12 @@
-import math
+data = [int(x) for x in open('input.txt').read().splitlines()]
 
 # Part 1
-data = open("./input.txt", "r").read().split("\n")
-
-fuelPart1 = 0
-
-for mass in data:
-    fuelPart1 += math.floor(int(mass) / 3) - 2
-
-
-print(fuelPart1)
-
+print(sum(x // 3 - 2 for x in data))
 
 # Part 2
-def calcFuel(mass):
-    fuel = math.floor(int(mass) / 3) - 2
+def calc_fuel(mass):
+    fuel = mass // 3 - 2
 
-    if fuel <= 0:
-        return 0
+    return fuel + calc_fuel(fuel) if fuel > 0 else 0
 
-    return fuel + calcFuel(fuel)
-
-
-fuelPart2 = 0
-for mass in data:
-    fuelPart2 += calcFuel(mass)
-
-print(fuelPart2)
+print(sum(calc_fuel(x) for x in data))
