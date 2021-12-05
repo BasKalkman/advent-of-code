afcf889db890345ae6a212fa8f6c420911b363fe
Bas <b_kalkman@hotmail.com>
2021-12-05 14:24:32 +0100

    Day 5 Part 2

--- 2021/Day05/solution.js
+++ 2021/Day05/solution.js
@@ -1,43 +1,49 @@
 const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8').split('\n')
 
-const parseAndSortNums = (line) => {
-  const [x1, y1, x2, y2] = line.split(/,| -> /).map(Number)
-  const xCoords = [x1,x2].sort((a,b) => a-b);
-  const yCoords = [y1,y2].sort((a,b) => a-b);
-  return {xCoords, yCoords}
+const parseNums = (line) => {
+  return [x1, y1, x2, y2] = line.split(/,| -> /).map(Number)
 }
 
-const coordsList = data.map(e => parseAndSortNums(e));
+const coordsList = data.map(e => parseNums(e));
 
 // Find highest coordintae points and make grid
 const [highestX, highestY] = coordsList.reduce((a,c) => {
-  const {xCoords,yCoords} = c;
-  if (xCoords[1] > a[0]) a[0] = xCoords[1];
-  if (yCoords[1] > a[1]) a[1] = yCoords[1];
+  const [x1, y1, x2, y2] = c;
+  if (x1 > a[0]) a[0] = x1
+  if (x2 > a[0]) a[0] = x2
+  if (y1 > a[1]) a[1] = y1
+  if (y2 > a[1]) a[1] = y2
   return a;
 }, [0,0])
 
-const grid = new Array(highestY).fill(0).map(() => {
-  return new Array(highestX).fill(0);
+const grid = new Array(highestY+1).fill(0).map(() => {
+  return new Array(highestX+1).fill(0);
 })
 
 // Fill numbers in grid
 for (const coords of coordsList) {
-  const {xCoords,yCoords} = coords;
-  const [xMin, xMax] = xCoords;
-  const [yMin, yMax] = yCoords;
+  const [x1, y1, x2, y2] = coords;
+  const xVector = x1 > x2 ? -1 : 1
+  const yVector = y1 > y2 ? -1 : 1
 
-  if (xMin===xMax || yMin === yMax) {
-    for (let y=yMin; y <= yMax; y++) {
-      for (let x=xMin; x <= xMax; x++) {
-        grid[y][x]++
+  if (x1===x2) {
+    for (let y=y1; y !== y2 + yVector; y += yVector) {
+        grid[y][x1]++
       }
+  } else if (y1===y2) {
+    for (let x=x1; x !== x2 + xVector; x += xVector) {
+      grid[y1][x]++
     }
-  }  
+  } else {
+    // Part 2
+    for (let x=x1, y=y1; x !== x2 + xVector; x+=xVector, y+=yVector) {
+      grid[y][x]++
+    }
+  } 
 }
 
 // Count overlaps
 const overlaps = grid.flat(2).filter(e => e > 1);
-console.log(`Part 1: ${overlaps.length}`)
+console.log(overlaps.length)
 
