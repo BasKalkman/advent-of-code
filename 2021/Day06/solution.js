b6411d585ea05cbdb65cbf16d055cb5dc27af8e4
Bas <b_kalkman@hotmail.com>
2021-12-06 09:54:35 +0100

    Day 6

--- 2021/Day06/solution.js
+++ 2021/Day06/solution.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8').split(',').map(Number)
 
-const schoolOfFish = [...data];
+const school = new Array(Math.max(...data)+1).fill(0);
+data.map((e) => {
+  school[e]++
+})
 
-for (let i=0; i < 80; i++) {
-  const tempFish = [];
-  for (let fish=0; fish < schoolOfFish.length; fish++) {
-    schoolOfFish[fish]--
-
-    if (schoolOfFish[fish] < 0) {
-      schoolOfFish[fish] = 6;
-      tempFish.push(8)
-    }
+for(let i = 0; i < 256; i++) {
+  if (i === 80) {
+    console.log(`Part 1: ${school.reduce((a,c) => a+c, 0)}`)
   }
+  
+  
+  const tempSchool = new Array(9).fill(0)
+  school.map((e,idx) => {
+    if (idx-1 < 0) {
+      tempSchool[6] += e;
+      tempSchool[8] += e;
+    } else {
+      tempSchool[idx-1] += e;
+    }
+  })
 
-  schoolOfFish.push(...tempFish)
-  tempFish.length = 0;
+  school.length = 0;
+  school.push(...tempSchool)
 }
-
-console.log(schoolOfFish.length)
+console.log(`Part 2: ${school.reduce((a,c) => a+c, 0)}`)
