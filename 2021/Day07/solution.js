2af3a8656ce595486d2b101baa057f35c0e67de1
Bas <b_kalkman@hotmail.com>
2021-12-07 10:04:22 +0100

    Day 7 - Refactor

--- 2021/Day07/solution.js
+++ 2021/Day07/solution.js
@@ -24,15 +24,18 @@ const getDiffs = (p2=false) => {
   return diffs;
 }
 
+const getSmallestDistance = (list) => {
+  const val = [...list.entries()].reduce((a,c) => {
+    return c[1] < a[1] ? c : a;
+  }, [0, Infinity])
+  return val[1]
+}
+
 const diffsP1 = getDiffs();
-const part1 = [...diffsP1.entries()].reduce((a,c) => {
-  return c[1] < a[1] ? c : a;
-}, [0, Infinity])
-console.log(`Part 1: ${part1[1]}`)
+const part1 = getSmallestDistance(diffsP1)
+console.log(`Part 1: ${part1}`)
 
 // Part 2
 const diffsP2 = getDiffs(true);
-const part2 = [...diffsP2.entries()].reduce((a,c) => {
-  return c[1] < a[1] ? c : a;
-}, [0, Infinity])
-console.log(`Part 2: ${part2[1]}`)
+const part2 = getSmallestDistance(diffsP2)
+console.log(`Part 2: ${part2}`)
