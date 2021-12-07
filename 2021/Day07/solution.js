ba8433c0092a72844bf77c439972aabbd9a8d60f
Bas <b_kalkman@hotmail.com>
2021-12-07 09:54:17 +0100

    Day 7

--- 2021/Day07/solution.js
+++ 2021/Day07/solution.js
@@ -2,16 +2,37 @@ const fs = require('fs')
 const data = fs.readFileSync('./input.txt', 'utf-8').split(',').map(Number);
 
 const positions = new Set(data);
-const diffs = new Map();
-for (const position of positions) {
-  const totalDistance = data.reduce((a,c) => {
-    const x = Math.abs(position - c)
-    return a + x;
-  },0)
-  diffs.set(position, totalDistance)
+
+const getDiffs = (p2=false) => {
+  const diffs = new Map();
+  for (const position of positions) {
+    const totalDistance = data.reduce((a,c) => {
+      let x = Math.abs(position - c)
+
+      if (p2) {
+        let toAdd = x;
+        for (let i=0; i < toAdd; i++) {
+          x += i;
+        }
+      }
+
+      return a + x;
+    },0)
+    diffs.set(position, totalDistance)
+  }
+
+  return diffs;
 }
 
-const part1 = [...diffs.entries()].reduce((a,c) => {
+const diffsP1 = getDiffs();
+const part1 = [...diffsP1.entries()].reduce((a,c) => {
   return c[1] < a[1] ? c : a;
 }, [0, Infinity])
-console.log(part1)
+console.log(`Part 1: ${part1[1]}`)
+
+// Part 2
+const diffsP2 = getDiffs(true);
+const part2 = [...diffsP2.entries()].reduce((a,c) => {
+  return c[1] < a[1] ? c : a;
+}, [0, Infinity])
+console.log(`Part 2: ${part2[1]}`)
