3dfd603887cd41f44e17a783952d4027b60877a4
Bas Kalkman <b_kalkman@hotmail.com>
2021-12-09 16:32:56 +0100

    Day 9

--- 2021/Day09/solution.js
+++ 2021/Day09/solution.js
@@ -1,25 +1,25 @@
 const fs = require('fs');
-const data = fs.readFileSync('./input.txt','utf-8').split(/\r?\n/).map(e => e.split(''));
-// const data = fs.readFileSync('./example.txt','utf-8').split(/\r?\n/).map(e => e.split(''));
+const data = fs.readFileSync('./input.txt', 'utf-8').split(/\r?\n/).map(e => e.split(''));
+// const data = fs.readFileSync('./example.txt', 'utf-8').split(/\r?\n/).map(e => e.split(''));
 
 const heightMap = new Map();
 for (let y = 0; y < data.length; y++) {
-  data[y].map((e,x) => heightMap.set(`${y}x${x}`, parseInt(e)))
+  data[y].map((e, x) => heightMap.set(`${y}x${x}`, parseInt(e)))
 }
-
 
 // Part 1
 const lowPoints = []
-for (const [k,v] of [...heightMap.entries()]) {
-  const diffs = [[-1,0], [0,-1], [0,1], [1,0]];
+const lowPointCoords = []
+for (const [k, v] of [...heightMap.entries()]) {
+  const diffs = [[-1, 0], [0, -1], [0, 1], [1, 0]];
   let isLowPoint = true;
   for (const diff of diffs) {
-    const [y,x] = k.split('x').map(Number)
-    const [dy,dx] = diff;
+    const [y, x] = k.split('x').map(Number)
+    const [dy, dx] = diff;
 
     // This works
-    if (heightMap.has(`${y+dy}x${x+dx}`)) {
-      const val = heightMap.get(`${y+dy}x${x+dx}`)
+    if (heightMap.has(`${y + dy}x${x + dx}`)) {
+      const val = heightMap.get(`${y + dy}x${x + dx}`)
       if (val <= v) {
         isLowPoint = false;
         break;
@@ -36,11 +36,42 @@ for (const [k,v] of [...heightMap.entries()]) {
     }
     */
   }
-  
+
   if (isLowPoint === true) {
-    lowPoints.push(v+1)
+    lowPoints.push(v + 1)
+    lowPointCoords.push(k);
   }
 }
 
-const resultP1 = lowPoints.reduce((a,c) => a + c, 0)
+
+const resultP1 = lowPoints.reduce((a, c) => a + c, 0)
 console.log(`Part 1: ${resultP1}`);
+
+// Part 2
+const seen = new Map();
+const getBasinSize = (coords) => {
+  let count = 0;
+  const [y, x] = coords.split('x').map(Number);
+
+  if (!heightMap.has(coords) || heightMap.get(coords) === 9) {
+    return count;
+  }
+
+  if (seen.has(coords)) {
+    return count;
+  }
+
+  count += 1;
+  seen.set(coords, 1);
+  const diffs = [[-1, 0], [0, -1], [1, 0], [0, 1]]
+  for (const diff of diffs) {
+    const [dy, dx] = diff;
+    count += getBasinSize(`${y + dy}x${x + dx}`);
+  }
+
+  return count;
+}
+
+const basinSizes = lowPointCoords.map(e => getBasinSize(e));
+const part2 = basinSizes.sort((a, b) => b - a).slice(0, 3).reduce((a, c) => a * c, 1)
+console.log(`Part 2: ${part2}`)
