02273c04bbb515a5b23fc853d6de47de84ea1a1c
Bas <b_kalkman@hotmail.com>
2021-12-12 16:12:04 +0100

    Day 12, Part 2

--- 2021/Day12/solution.js
+++ 2021/Day12/solution.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8').split(/\r?\n/);
 // const data = fs.readFileSync('./example.txt', 'utf-8').split(/\r?\n/);
+// const data = fs.readFileSync('./example2.txt', 'utf-8').split(/\r?\n/);
 
 // Map connections
-const connections = { 'end': []}
+const connections = {}
 data.map(e => {
   const [from, to] = e.split('-')
 
@@ -20,24 +21,33 @@ data.map(e => {
 })
 
 // Check number of routes from start
-const findRoutesFrom = (node, seen) => {
+const findRoutesFrom = (node, seen, mayDouble) => {
   if (node === 'end') {
     return 1;
   }
+  let double = mayDouble;
 
 
   if ((node.toLowerCase() === node) && seen.has(node)) {
-    return 0;
+    if (!mayDouble) {
+      return 0;
+    } else if (node === 'start') {
+      return 0;
+    } else {
+      double = false
+    }
   }
   
   seen.add(node)
   let result = 0;
   for (nextNode of connections[node]) {
-      result += findRoutesFrom(nextNode, new Set([...seen])) // New set  so visited down one path, does not preclude another unvisited one.
+      result += findRoutesFrom(nextNode, new Set([...seen]), double) // New set so visited down one path, does not preclude another unvisited one.
   }
 
   return result;
 }
 
-const routesFromStart = findRoutesFrom('start', new Set())
+const routesFromStart = findRoutesFrom('start', new Set(), false)
 console.log(routesFromStart)
+const routesWithDuplicate = findRoutesFrom('start', new Set(), true);
+console.log(routesWithDuplicate)
