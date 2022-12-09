ca8794bdb244aaf37e36292b8c92ea2215eaf90a
Bas Kalkman <b_kalkman@hotmail.com>
2022-12-09 12:17:52 +0100

    2022 Day 8

--- 2022/Day08/solution.js
+++ 2022/Day08/solution.js
@@ -24,7 +24,6 @@ const checkTree = (y, x) => {
     // TODO: Why length-1 here but not on x axis?
     while (tmpY < trees.length - 1) {
         tmpY++;
-        console.log(tmpY);
         if (trees[tmpY][tmpX] >= height) {
             visibleBottom = false;
             break;
@@ -60,4 +59,50 @@ for (let y = 0; y < trees.length; y++) {
         if (checkTree(y, x)) visibleCount++;
     }
 }
-console.log(visibleCount);
+console.log(`Part 1: ${visibleCount}`);
+
+// Part 2
+const getScenicScore = (y, x) => {
+    const height = trees[y][x];
+    let up = 0;
+    let down = 0;
+    let left = 0;
+    let right = 0;
+    let tmpY = y;
+    let tmpX = x;
+
+    while (tmpY > 0) {
+        up++;
+        tmpY--;
+        if (trees[tmpY][tmpX] >= height) break;
+    }
+    tmpY = y;
+    while (tmpY < trees.length - 1) {
+        down++;
+        tmpY++;
+        if (trees[tmpY][tmpX] >= height) break;
+    }
+    tmpY = y;
+    while (tmpX > 0) {
+        left++;
+        tmpX--;
+        if (trees[tmpY][tmpX] >= height) break;
+    }
+    tmpY = y;
+    tmpX = x;
+    while (tmpX < trees[tmpY].length) {
+        right++;
+        tmpX++;
+        if (trees[tmpY][tmpX] >= height) break;
+    }
+
+    return up * down * left * right;
+};
+
+const counts = [];
+for (let y = 0; y < trees.length; y++) {
+    for (let x = 0; x < trees[y].length; x++) {
+        counts.push(getScenicScore(y, x));
+    }
+}
+console.log(`Part 2: ${Math.max(...counts)}`);
