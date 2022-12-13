bf65aff56a263ae392accf7971aba7f34d49142f
Bas <b_kalkman@hotmail.com>
2022-12-13 12:52:50 +0100

    2022 Day 10

--- 2022/Day10/solution.js
+++ 2022/Day10/solution.js
@@ -6,8 +6,15 @@ const cycles = [20,60,100,140,180,220];
 let register = 1;
 let cycle = 0;
 let values = [];
+let lines = [];
 
 const addCycle = () => {
+    let tmpCycle = cycle % 40;
+    if ([tmpCycle-1,tmpCycle,tmpCycle+1].includes(register)) {
+        lines.push('#')
+    } else {
+        lines.push('.');
+    }
     cycle++
     if (cycles.includes(cycle)) {
         values.push(register * cycle);
@@ -25,3 +32,21 @@ for (const line of data) {
 }
 const part1 = values.reduce((a,c) => a + c, 0);
 console.log(part1);
+
+// Part 2
+let resultLines = []
+let tempLine = ''
+for (let i = 0; i < lines.length; i++) {
+    if (i > 0 && i % 40 === 0) {
+        resultLines.push(tempLine);
+        tempLine = '';
+    }
+    tempLine += lines[i]
+}
+resultLines.push(tempLine)
+
+console.log('Part 2:')
+resultLines.map(e => {
+    let str = e.replace(/\./g, ' ');
+    console.log(str);
+})
