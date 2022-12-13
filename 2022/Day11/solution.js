6fe833ca772c0b2d4eb2b0d978d316057c181d5f
Bas Kalkman <b_kalkman@hotmail.com>
2022-12-13 18:03:43 +0100

    2022 Day 11 Part 1

--- 2022/Day11/solution.js
+++ 2022/Day11/solution.js
@@ -1,15 +1,18 @@
 const fs = require('fs');
-const data = fs.readFileSync('./test.txt', 'utf-8').split('\n\n')
-// const data = fs.readFileSync('./input.txt', 'utf-8').split('\n\n')
+// const data = fs.readFileSync('./test.txt', 'utf-8').split('\n\n');
+const data = fs.readFileSync('./input.txt', 'utf-8').split('\n\n');
 
 class Monkey {
     constructor(monkey) {
-        this.name = monkey.match(/Monkey \d+/g)[0]
-        this.items = monkey.match(/Starting items: (.+)/g)[0].match(/\d+/g).map(Number);
-        this.op = monkey.match(/Operation: new = (.+)/)[1]
+        this.name = monkey.match(/Monkey \d+/g)[0];
+        this.items = monkey
+            .match(/Starting items: (.+)/g)[0]
+            .match(/\d+/g)
+            .map(Number);
+        this.op = monkey.match(/Operation: new = (.+)/)[1];
         this.test = parseInt(monkey.match(/Test: .+ (\d+)/)[1]);
-        this.trueMonkey = parseInt(monkey.match(/If true: .+ (\d+)/)[1])
-        this.falseMonkey = parseInt(monkey.match(/If false: .+ (\d+)/)[1])
+        this.trueMonkey = parseInt(monkey.match(/If true: .+ (\d+)/)[1]);
+        this.falseMonkey = parseInt(monkey.match(/If false: .+ (\d+)/)[1]);
         this.inspectionCount = 0;
     }
 
@@ -18,9 +21,7 @@ class Monkey {
         let object = this.items.shift();
         object = this.testObject(object);
         object = Math.floor(object / 3);
-        
 
-        
         if (object % this.test === 0) {
             monkeys[this.trueMonkey].grab(object);
         } else {
@@ -32,8 +33,6 @@ class Monkey {
         let op = this.op.replace(/old/g, object);
         let value = eval(op);
         return value;
-        let result = value % this.test === 0;
-        return result;
     }
 
     grab(object) {
@@ -47,12 +46,11 @@ class Monkey {
     }
 }
 
-
-
-const monkeys = data.map(e => new Monkey(e))
+const monkeys = data.map((e) => new Monkey(e));
 
 for (let i = 0; i < 20; i++) {
-    monkeys.map(e => e.runTurn())
+    monkeys.map((e) => e.runTurn());
 }
-const inspectionCounts = monkeys.map(e => e.inspectionCount).sort((a,b) => a - b)
+const inspectionCounts = monkeys.map((e) => e.inspectionCount).sort((a, b) => b - a);
+console.log(inspectionCounts);
 console.log(inspectionCounts[0] * inspectionCounts[1]);
