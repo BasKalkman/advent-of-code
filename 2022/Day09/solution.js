ea6164087aace9aa02f1c717482eec1b6a162835
Bas <b_kalkman@hotmail.com>
2022-12-13 10:04:15 +0100

    2022 Day 9

--- 2022/Day09/solution.js
+++ 2022/Day09/solution.js
@@ -16,6 +16,10 @@ class Tail {
 		return `x${this.x}y${this.y}`;
 	}
 
+	coords() {
+		return [this.x, this.y];
+	}
+
 	moveToHead(hx, hy) {
 		if (Math.abs(hx - this.x) > 1 || Math.abs(hy - this.y) > 1) {
 			if (this.x === hx) {
@@ -43,4 +47,30 @@ for (const instruction of data) {
 		tail.moveToHead(head.x, head.y);
 	}	
 }
-console.log(tail.visited.size);
+console.log('Part 1: ', tail.visited.size);
+
+// Part 2
+head.x = 0;
+head.y = 0;
+const tails = []
+for (let i = 0; i < 9; i++) {
+	tails.push(new Tail())
+}
+
+for (const instruction of data) {
+	const [dir, num] = instruction.split(/\W/g);
+	for (let i = 0; i < parseInt(num); i++) {
+		if (dir === 'U') head.y++
+		if (dir === 'D') head.y--
+		if (dir === 'L') head.x--
+		if (dir === 'R') head.x++
+		tails[0].moveToHead(head.x, head.y);
+		for (let j = 1; j < tails.length; j++) {
+			const [tx, ty] = tails[j-1].coords();
+			tails[j].moveToHead(tx, ty);
+		}
+	}	
+}
+
+const part2 = tails[tails.length - 1].visited.size;
+console.log('Part 2: ', part2)
