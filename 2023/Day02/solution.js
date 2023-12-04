f1bc5474d02051aa0efb863d662b797bedfb5bff
Bas Kalkman <b_kalkman@hotmail.com>
2023-12-05 00:32:37 +0100

    2023 Day 2

---
+++ 2023/Day02/solution.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const data = fs.readFileSync('./input.txt', 'utf8').split('\n');
+
+// Part 1
+const maxRed = 12;
+const maxGreen = 13;
+const maxBlue = 14;
+const games = data.map((e) => {
+    // console.log(/(\d+) red/gi.exec(e));
+    const reds = [...e.matchAll(/(\d+) red/gi)].reduce((a, c) => {
+        return a === null || parseInt(c[1]) > a ? parseInt(c[1]) : a;
+    }, null);
+    const blues = [...e.matchAll(/(\d+) blue/gi)].reduce((a, c) => {
+        return a === null || parseInt(c[1]) > a ? parseInt(c[1]) : a;
+    }, null);
+    const greens = [...e.matchAll(/(\d+) green/gi)].reduce((a, c) => {
+        return a === null || parseInt(c[1]) > a ? parseInt(c[1]) : a;
+    }, null);
+
+    return {
+        red: reds,
+        blue: blues,
+        green: greens,
+        game: parseInt(/Game (\d+)/gi.exec(e)[1]),
+    };
+});
+const part1 = games.reduce((a, c) => {
+    if (c.red <= maxRed && c.blue <= maxBlue && c.green <= maxGreen) {
+        return a + c.game;
+    }
+    return a;
+}, 0);
+console.log('Part 1: ', part1);
