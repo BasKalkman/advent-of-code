1a4ed944786a77e0728bf72dce47d9e501b300a3
Bas Kalkman <b_kalkman@hotmail.com>
2024-12-03 13:53:39 +0100

    Day 2, Part 2

--- 2024/Day02/solution.js
+++ 2024/Day02/solution.js
@@ -1,13 +1,14 @@
 const fs = require('fs');
 const data = fs.readFileSync('./input.txt', 'utf-8').split('\n');
 
-const result = data.filter(e => {
-    const arr = e.split(" ").map(Number);
+
+const checker = (input) => {
+    const arr = input.split(" ").map(Number);
     const direction = arr[1] > arr[0] ? 'up' : 'down';
-    const result = arr.reduce((a,c,i) => {
+    const result = arr.reduce((a, c, i) => {
         if (i === 0) return true;
-        if (a ===  false) return a;
-        const sum = c - arr[i-1];
+        if (a === false) return a;
+        const sum = c - arr[i - 1];
         if (direction === 'up') {
             if (sum < 1 || sum > 3) return false;
         }
@@ -17,6 +18,22 @@ const result = data.filter(e => {
         return a;
     }, true)
     return result;
-})
+}
+
+const result = data.filter(e => checker(e))
 console.log('Part 1: ', result.length);
 // Part 2
+const result2 = data.filter(e => {
+    if (checker(e)) return true;
+    const arr = e.split(' ').map(Number);
+    for (let i = 0; i < arr.length; i++) {
+        const testArr = arr.filter((num, idx) => {
+            if (idx === i) return false;
+            return true;
+        })
+        const testResult = checker(testArr.join(' '))
+        if (testResult) return true;
+    }
+    return false;
+})
+console.log('Part 2: ', result2.length);
